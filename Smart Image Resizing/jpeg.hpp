a783fc219654edb767346ac72db193f88f2a9fc8
Sarah <158523291+ssaze@users.noreply.github.com>
2024-10-06 18:07:47 -0400

    Update jpeg.hpp

--- Smart Image Resizing/jpeg.hpp
+++ Smart Image Resizing/jpeg.hpp
@@ -1,16 +1,6 @@
 #ifndef JPEG_HPP
 #define JPEG_HPP
 
-/**
- * jpeg.hpp
- *
- * Provides functions that interface with libjpeg or libjpeg-turbo to
- * read or write jpeg files to or from Image objects.
- *
- * Author: Amir Kamil <akamil@umich.edu>
- * Date: 2024-03-30
- */
-
 #include <cctype>
 #include <cstdio>
 #include <iostream>
@@ -25,15 +15,13 @@
 //          ignoring capitalization.
 bool has_jpeg_extension(const std::string &filename);
 
-// REQUIRES: image points to an Image object
-// MODIFIES: *image, cout
+
 // EFFECTS:  Reads a data from the JPEG file specified by the given
 //           filename into the given Image object.
 //           Returns whether or not the file was successfully read.
 bool read_jpeg(Image *image, const std::string &filename);
 
-// REQUIRES: image points to a valid Image object
-// MODIFIES: the file corresponding to the given filename, cout
+
 // EFFECTS:  Writes the data in the given Image object into the file
 //           of the given name, in JPEG format.
 //           high_quality specifies whether to use low- or
@@ -72,12 +60,7 @@ bool has_jpeg_extension(const std::string &filename) {
 
 #if JPEG_HPP_USE_LIBJPEG
 
-// REQUIRES: buffer refers to an array that is at least as large as
-//           Image_width(image) if grayscale is true, or 3 *
-//           Image_width(image) if it is false;
-//           image points to a valid Image;
-//           0 <= row < Image_height(image)
-// MODIFIES: *image
+
 // EFFECTS:  Copies the pixels from buffer into image at the given
 //           row. If grayscale is true, then each element of buffer is
 //           the value for all three colors of a pixel. Otherwise, the
@@ -98,10 +81,7 @@ static void copy_pixels(const Buffer &buffer, Image *image, int row,
   }
 }
 
-// REQUIRES: info_p points to a valid jpeg_decompress_struct, with the
-//           header already read and decompression started;
-//           image points to a valid Image of the appropriate size
-// MODIFIES: *image
+
 // EFFECTS:  Reads the data from the image source represented by
 //           info_p into the given Image.
 static void read_jpeg8(j_decompress_ptr info_p, Image *image) {
@@ -117,11 +97,7 @@ static void read_jpeg8(j_decompress_ptr info_p, Image *image) {
   }
 }
 
-// REQUIRES: image points to a valid Image of the appropriate size;
-//           buffer refers to an array that is at least as large as
-//           3 * Image_width(image);
-//           0 <= row < Image_height(image)
-// MODIFIES: the contents of buffer
+
 // EFFECTS:  Reads a full row from image into buffer. Each color value
 //           is truncated by the value of Mask.
 template<int Mask, typename Buffer>
@@ -134,10 +110,7 @@ static void get_row(const Image *image, Buffer &buffer, int row) {
   }
 }
 
-// REQUIRES: info_p points to a valid jpeg_compress_struct, with the
-//           metadata already set and compression started;
-//           image points to a valid Image of the appropriate size
-// MODIFIES: the data target represented by info_p
+
 // EFFECTS:  Writes the data from the given Image to the data target
 //           represented by info_p.
 static void write_jpeg8(j_compress_ptr info_p, const Image *image) {
@@ -153,8 +126,7 @@ static void write_jpeg8(j_compress_ptr info_p, const Image *image) {
   }
 }
 
-// REQUIRES: image points to an Image object
-// MODIFIES: *image, cout
+
 // EFFECTS:  Reads a data from the JPEG file specified by the given
 //           filename into the given Image object.
 //           Returns whether or not the file was successfully read.
@@ -196,8 +168,7 @@ bool read_jpeg(Image *image, const std::string &filename) {
   return true;
 }
 
-// REQUIRES: image points to a valid Image object
-// MODIFIES: the file corresponding to the given filename, cout
+
 // EFFECTS:  Writes the data in the given Image object into the file
 //           of the given name, in JPEG format.
 //           high_quality specifies whether to use low- or
@@ -252,7 +223,6 @@ static constexpr const char *MISSING_LIBJPEG =
   "See the project specification for more details.\n"
   "=========================================================================";
 
-// MODIFIES: cout
 // EFFECTS:  Prints an error message with instructions of how to
 //           compile with libjpeg/libjpeg-turbo and returns false.
 bool read_jpeg(Image *, const std::string &) {
@@ -260,7 +230,6 @@ bool read_jpeg(Image *, const std::string &) {
   return false;
 }
 
-// MODIFIES: cout
 // EFFECTS:  Prints an error message with instructions of how to
 //           compile with libjpeg/libjpeg-turbo and returns false.
 bool write_jpeg(const Image *, const std::string &, bool) {
